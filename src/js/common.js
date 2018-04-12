720b009774f5b5e119c1cb179d6364e970312eea
ZJ <zj781119282@163.com>
2018-04-12 19:20:14 +0800

    first commit

---
+++ src/js/common.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2018/4/12 0012.
+ */
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'header/header';
+import 'header/header.scss';
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+/**
+ * 官网首页js
+ */
+
+import $ from 'jquery';
+
+import common from './common';
+import './../css/index.scss';
